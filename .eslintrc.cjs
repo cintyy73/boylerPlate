8f95a258b856502bb6b9adf85ae072fe2576b553
cintyy <cintiapaezdm@gmail.com>
2023-05-01 18:45:55 -0300

    Eslint-prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,6 +12,7 @@ module.exports = {
   plugins: ["react-refresh", "simple-import-sort"],
   rules: {
     "react-refresh/only-export-components": "warn",
+    "react/prop-types": "off",
     "prettier/prettier": ["warn", { endOffLine: "auto" }],
     "simple-import-sort/imports": "warn",
     "simple-import-sort/exports": "warn",
